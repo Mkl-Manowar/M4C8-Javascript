e8dff034cc66a68398f50861fbdd7da339d0350c
Mkl-Manowar <151755301+Mkl-Manowar@users.noreply.github.com>
2024-06-04 08:40:46 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// Bucle for
+const myList = ["velma", "scout", "jane", "john", "harry"]
+
+for (let i = 0; i < myList.length; i++)
+  console.log(myList[i])
+
+
+// Bucle while
+const i = 0
+
+while (i < myList.length; i++)
+  console.log(myList[i])
+
+
+// Función flecha
+
+saludo = () => {
+  return "Hola Mundo!"
+}
